7dda7f4a7efdfeb54e15b6c0a23848b3dac1cdb0
RobertAKARobin <robertgfthomas@gmail.com>
2015-12-06 23:45:46 -0500

    unannotated version

--- app.js
+++ app.js
@@ -8,103 +8,51 @@ var env = require("./env");
 
 app.set("view engine", "hbs");
 
-console.log("* Restarting! This means all session data is erased.");
-
-app.use(function(req, res, next){
-  console.log("------");
-  next();
-});
-
-var cookieParserFunction = cookieParser();
-app.use(function(req, res, next){
-  console.log("* Getting the session ID from cookies...");
-  cookieParserFunction(req, res, next);
-});
-
-var sessionFunction = session({
+app.use(cookieParser());
+app.use(session({
   secret: env.sessionSecret,
   resave: true,
   saveUninitialized: true
-});
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(function(req, res, next){
-  console.log("* Parsing the session variables...");
-  sessionFunction(req, res, next);
-});
-
-var passportInitializer = passport.initialize();
-app.use(function(req, res, next){
-  console.log("* Initializing Passport...");
-  passportInitializer(req, res, next);
-});
-
-var passportSessionFunction = passport.session();
-app.use(function(req, res, next){
-  console.log("* Setting Passport's own session variables...");
-  passportSessionFunction(req, res, next);
-});
-
-app.use(function(req, res, next){
-  if(req.user){
-    console.log("* Making " + req.user.username + "'s deserialized user data available in .hbs views...");
-  }else{
-    console.log("* Skipping a step because there's no current user...")
-  }
   res.locals.user = req.user;
   next();
 });
 
 passport.serializeUser(function(user, next) {
-  console.log("* Serializing the user: Saving " + user.username + "'s data as a session variable...");
   next(null, user);
 });
-
 passport.deserializeUser(function(user, next) {
-  console.log("* Deserializing the user: Getting " + user.username + "'s data from session variables...");
   next(null, user);
 });
-
-var twitterStrategy = new Strategy(
+passport.use(new Strategy(
   env.twitter,
   function(token, tokenSecret, profile, next){
-    console.log("* Got " + profile.username + "'s info! Sending it to be serialized...");
     next(null, profile);
   }
-);
-passport.use(twitterStrategy);
-
-// Moving on to the routes...
+));
 
 app.get("/", function(req, res){
-  console.log("* Rendering the index...");
   res.render("index");
 });
 
 app.get("/cat", function(req, res){
-  console.log("* #cat: Rendering a cat picture.");
   res.render("show");
 });
 
-var twitterAuthenticator = passport.authenticate("twitter");
-app.get("/signin", function(req, res){
-  console.log("* #signin: Redirecting to Twitter...");
-  twitterAuthenticator(req, res);
-});
+app.get("/signin", passport.authenticate("twitter"));
 
 app.get("/signout", function(req, res){
-  console.log("* #signout: Deleting " + (req.user ? user.username : "user") + "'s session variables and making sure their info isn't being saved in memory anywhere...");
   req.session.destroy();
   res.locals.user = null
   res.render("signout");
 });
 
-var authenticateNewUser = passport.authenticate("twitter", { failureRedirect: "/signout" });
 app.get("/twitter-callback",
-  function(req, res, next){
-    console.log("* Using the OAuth tokens Twitter sent back to get the user's Twitter profile information...")
-    authenticateNewUser(req, res, next);
-  },
+  passport.authenticate("twitter", { failureRedirect: "/signout" }),
   function(req, res){
-    console.log("* Redirecting to the main page...");
     res.redirect("/");
   });
 
